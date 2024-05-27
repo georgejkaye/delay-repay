56c5751b4d44b1f593bed2dd56b186ead8f9630e
George Kaye <georgejkaye@gmail.com>
2024-05-27 21:48:09 +0100

    [feat] Add delay calculator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,163 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
+import { act, useEffect, useState } from "react"
+
+const TopBar = () => (
+  <div className="bg-blue-700 p-4">
+    <h1 className="font-bold text-2xl">Delay repay calculator</h1>
+  </div>
+)
+
+type SetState<T> = React.Dispatch<React.SetStateAction<T>>
+
+const getDateFromText = (str: string) => {
+  let year = Number.parseInt(str.substring(0, 4))
+  let month = Number.parseInt(str.substring(5, 7))
+  let day = Number.parseInt(str.substring(8, 10))
+  if (
+    !Number.isNaN(year) &&
+    !Number.isNaN(month) &&
+    !Number.isNaN(day) &&
+    year >= 0 &&
+    month >= 0 &&
+    month <= 12 &&
+    day >= 0 &&
+    day <= 31
+  ) {
+    return { year, month, day }
+  } else {
+    return undefined
+  }
+}
+
+const getTimeFromText = (str: string) => {
+  let minuteIndex = str.length === 4 ? 2 : 3
+  let hour = Number.parseInt(str.substring(0, 2))
+  let minute = Number.parseInt(str.substring(minuteIndex, minuteIndex + 2))
+  if (
+    !Number.isNaN(hour) &&
+    !Number.isNaN(minute) &&
+    hour >= 0 &&
+    hour < 24 &&
+    minute >= 0 &&
+    minute < 60
+  ) {
+    return { hour, minute }
+  } else {
+    return undefined
+  }
+}
+
+const DateAndTimePicker = (props: {
+  date: Date | undefined
+  setDate: SetState<Date | undefined>
+}) => {
+  const [dateText, setDateText] = useState(
+    props.date
+      ? `${props.date.getFullYear().toString().padStart(4, "0")}-${props.date
+          .getMonth()
+          .toString()
+          .padStart(2, "0")}-${props.date
+          .getDate()
+          .toString()
+          .padStart(2, "0")}`
+      : ""
+  )
+  const [timeText, setTimeText] = useState(
+    props.date
+      ? `${props.date.getHours().toString().padStart(2, "0")}:${props.date
+          .getMinutes()
+          .toString()
+          .padStart(2, "0")}`
+      : ""
+  )
+  useEffect(() => {
+    let dateResult = getDateFromText(dateText)
+    let timeResult = getTimeFromText(timeText)
+    if (!dateResult || !timeResult) {
+      props.setDate(undefined)
+    } else {
+      let { year, month, day } = dateResult
+      let { hour, minute } = timeResult
+      props.setDate(new Date(year, month, day, hour, minute))
+    }
+  }, [dateText, timeText])
+  const onChangeText = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    setState: SetState<string>
+  ) => {
+    setState(e.target.value)
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex flex-row gap-4">
+      <input
+        aria-label="Date"
+        type="date"
+        className="text-black p-4 rounded-xl w-48 text-center"
+        value={dateText}
+        onChange={(e) => onChangeText(e, setDateText)}
+      />
+      <input
+        aria-label="Time"
+        type="time"
+        className="text-black p-4 rounded-xl w-24 text-center"
+        value={timeText}
+        onChange={(e) => onChangeText(e, setTimeText)}
+      />
+    </div>
+  )
+}
+
+const MainSection = () => {
+  const [expected, setExpected] = useState<Date | undefined>(new Date())
+  const [actual, setActual] = useState<Date | undefined>(new Date())
+  const [delay, setDelay] = useState<number | undefined>(undefined)
+  const getDelayText = (delay: number) =>
+    delay < 15
+      ? "Less than 15 minutes"
+      : delay < 30
+      ? "15-29 minutes"
+      : delay < 60
+      ? "30-59 minutes"
+      : delay < 120
+      ? "60-119 minutes"
+      : "Over 120 minutes"
+  useEffect(() => {
+    if (!expected || !actual) {
+      setDelay(undefined)
+    } else {
+      setDelay((actual.getTime() - expected.getTime()) / 1000 / 60)
+    }
+  }, [expected, actual])
+  return (
+    <div className="m-5 w-mobileContent tablet:w-tabletContent desktop:w-content mx-auto">
+      <div>
+        <h2 className="font-bold text-xl">Delay details</h2>
+        <div className="flex flex-col tablet:flex-row gap-5">
+          <div>
+            <div className="mt-4 mb-2">Expected arrival</div>
+            <DateAndTimePicker date={expected} setDate={setExpected} />
+          </div>
+          <div>
+            <div className="mt-4 mb-2">Expected arrival</div>
+            <DateAndTimePicker date={actual} setDate={setActual} />
+          </div>
+        </div>
+        <div className="my-4 text-lg">
+          {!delay
+            ? "Enter expected and actual arrival times above"
+            : `Delayed ${delay} minutes: ${getDelayText(delay)}`}
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    </div>
+  )
 }
+
+const Home = () => (
+  <main>
+    <TopBar />
+    <MainSection />
+  </main>
+)
+
+export default Home
